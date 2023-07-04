c993c9005ea4c9e0f80f43f49378ff5e6a060598
Rehman Gull <experts-web2@gmail.com>
2023-07-04 16:29:15 +0500

    folder structure improvement + layout setting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from 'src/shared/shared.module';
+import { SharedModule } from 'src/shared/modules/shared.module';
 import { HomeComponent } from './components/home/home.component';
 import { BlogsComponent } from './components/blogs/blogs.component';
 import { UsersComponent } from './components/users/users.component';
